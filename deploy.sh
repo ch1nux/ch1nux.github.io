acfb842adbee174440a2c4e2fe1cabbbd2b96bfd
Jhony Grillet <jgrillet@mppp.gob.ve>
2020-05-29 14:06:59 -0400

    Actualización 29-05-2020 02:06pm

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,19 @@
 # Abort on errors
 set -e
 
-# Make sure you are on the right branch
-git checkout gh-pages
-
 # Build the page
 vuepress build
 
+# Change to directory
 cd .vuepress/dist
 
+# Create CNAME for automatic DNS redirection
 echo "www.chinux.tech" > CNAME
 
+# Forced push to different remote
 git add -A
 git commit -m "Actualización $(date '+%d-%m-%Y %I:%M%p')"
 git push -f https://github.com/chinuxparibus/chinuxparibus.github.io.git master
 
 cd -
+rm -rf .vuepress/dist
