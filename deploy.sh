fe53d432681777c458a4b95b167dbf649552a7c9
Jhony Grillet <jgrillet@mppp.gob.ve>
2020-06-02 11:09:52 -0400

    Depurar repositorio para nuevos deployments

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,15 @@ set -e
 vuepress build
 
 # Change to directory
-cd .vuepress/dist
+cd .vuepress/dist/
 
 # Create CNAME for automatic DNS redirection
 echo "www.chinux.tech" > CNAME
 
 # Forced push to different remote
-git add -A
+git add .
 git commit -m "Actualización $(date '+%d-%m-%Y %I:%M%p')"
-git push -f https://github.com/chinuxparibus/chinuxparibus.github.io.git master
+git push -f https://github.com/chinuxparibus/chinuxparibus.github.io master
 
 cd -
 rm -rf .vuepress/dist
